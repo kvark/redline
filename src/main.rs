18d9a96ed8e8047cc3aac9884f377a31e85368e7
Dzmitry Malyshau <kvark@fastmail.com>
2026-08-21 06:46:15 -0700

    Fix chase camera: add split modules + config defaults (part 1/2)

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,85 @@
-TOO_LARGE_SEE_LOCAL_COMMIT_5a1fbb9
+#![allow(
+    irrefutable_let_patterns,
+    clippy::new_without_default,
+    clippy::needless_borrowed_reference
+)]
+#![warn(
+    trivial_casts,
+    trivial_numeric_casts,
+    unused_extern_crates,
+    unused_qualifications,
+    clippy::pattern_type_mismatch
+)]
+
+mod config;
+mod game;
+mod helpers;
+mod glb;
+mod planet;
+mod race;
+mod vehicle;
+
+use game::{Game, QuitEvent};
+
+struct App {
+    game: Option<Game>,
+}
+
+impl winit::application::ApplicationHandler for App {
+    fn resumed(&mut self, event_loop: &winit::event_loop::ActiveEventLoop) {
+        self.game = Some(Game::new(event_loop));
+    }
+
+    fn about_to_wait(&mut self, _event_loop: &winit::event_loop::ActiveEventLoop) {
+        if let Some(game) = self.game.as_ref() {
+            game.window.request_redraw();
+        }
+    }
+
+    fn window_event(
+        &mut self,
+        event_loop: &winit::event_loop::ActiveEventLoop,
+        _window_id: winit::window::WindowId,
+        event: winit::event::WindowEvent,
+    ) {
+        let game = self.game.as_mut().unwrap();
+        match game.on_event(&event) {
+            Ok(control_flow) => event_loop.set_control_flow(control_flow),
+            Err(QuitEvent) => event_loop.exit(),
+        }
+    }
+}
+
+fn main() {
+    #[cfg(target_arch = "wasm32")]
+    {
+        console_error_panic_hook::set_once();
+        let _ = console_log::init_with_level(log::Level::Info);
+        mount_embedded_assets();
+    }
+    #[cfg(not(target_arch = "wasm32"))]
+    {
+        env_logger::init();
+    }
+
+    let event_loop = winit::event_loop::EventLoop::new().unwrap();
+    event_loop.set_control_flow(winit::event_loop::ControlFlow::Poll);
+    let mut app = App { game: None };
+    event_loop.run_app(&mut app).unwrap();
+}
+
+#[cfg(target_arch = "wasm32")]
+fn mount_embedded_assets() {
+    use include_dir::{Dir, include_dir};
+    static ASSETS: Dir = include_dir!("$CARGO_MANIFEST_DIR/assets");
+    let root = std::path::PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("assets");
+    fn walk(dir: &Dir, root: &std::path::Path) {
+        for file in dir.files() {
+            blade_engine::vfs::mount(root.join(file.path()), file.contents().to_vec());
+        }
+        for child in dir.dirs() {
+            walk(child, root);
+        }
+    }
+    walk(&ASSETS, &root);
+}
